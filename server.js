966a3020d2cf0ee7aaad95cd72518799f445719c
uksharma <mysrms2021@gmail.com>
2024-08-28 20:06:11 +0530

    fixing error

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
@@ -7,40 +9,42 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
 
-//configure env
+// Configure env
 dotenv.config();
 
-//db config
+// DB config
 connectDB();
 
-//rest object
+// ES module fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Rest object
 const app = express();
 
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-//routes
+// Routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
-app.use("*", function (req, res) {
+// Serve React App
+app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-//Port
+// Port
 const PORT = process.env.PORT || 8080;
 
-//run listen
+// Run server
 app.listen(PORT, () => {
   console.log(
-    `Server Running ${process.env.DEV_MODE} on mode on port ${PORT}`.bgCyan
-      .white
+    `Server Running ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white
   );
 });
