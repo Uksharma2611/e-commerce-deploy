55fc88056d332ed5bfdeb98853d8d460b224dd32
uksharma <mysrms2021@gmail.com>
2024-08-28 20:27:38 +0530

    rolled back

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 // Routes
 app.use("/api/v1/auth", authRoutes);
@@ -36,7 +36,7 @@ app.use("/api/v1/product", productRoutes);
 
 // Serve React App
 app.use("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // Port
