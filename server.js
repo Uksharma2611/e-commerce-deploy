47dd6195a7d859ada25c27e4217918e4bcfad5ea
uksharma <mysrms2021@gmail.com>
2024-09-24 11:05:59 +0530

    fixed various functionalities

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
@@ -9,42 +7,45 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
-// Configure env
+//configure env
 dotenv.config();
 
-// DB config
+//db config
 connectDB();
 
-// ES module fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+//esmodule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
-// Rest object
+//rest object
 const app = express();
 
-// Middleware
+//middleware
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-// Routes
+//routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-// Serve React App
-app.use("*", (req, res) => {
+//rest api
+app.use("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-// Port
+//Port
 const PORT = process.env.PORT || 8080;
 
-// Run server
+//run listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white
+    `Server Running on port ${PORT}`.bgCyan
+      .white
   );
 });
